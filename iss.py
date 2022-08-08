33483503cd98b3386339783a168bfbfded706392
spaceybread <Spaceybread@gmail.com>
2022-08-09 02:14:23 +0530

    Add files via upload

---
+++ iss.py
@@ -0,0 +1,33 @@
+from urllib.request import urlopen
+import json
+from datetime import *
+import pytz
+from tzwhere import tzwhere
+from timezonefinder import TimezoneFinder
+
+
+while True:
+
+    tzf = TimezoneFinder()
+
+    req = urlopen("http://api.open-notify.org/iss-now.json").read()
+
+    obj = json.loads(req)
+
+    latt = float(obj['iss_position']['latitude'])
+    long = float(obj['iss_position']['longitude'])
+
+    timezoneISS = tzf.timezone_at(lat=latt, lng=long)
+
+    print(obj['timestamp'])
+    print(str(latt) + ', ' + str(long))
+
+    print(str(timezoneISS))
+
+    localTime = datetime.now(pytz.timezone(timezoneISS))
+    print('Local time: {}:{}'.format(localTime.hour,localTime.minute))
+
+    if int(localTime.hour) <= 6 or int(localTime.hour) >= 19:
+        print('The ISS is cruising through the dark night! Sweet trails!')
+    else:
+        print("The ISS is riding across the bright sky! It's always sunny up there!")
